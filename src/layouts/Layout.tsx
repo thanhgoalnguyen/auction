842829c49689810f0248615df089a620bf7af448
@nguyenngocthanh137 <nguyenngocthanh137@gmail.com>
2025-12-31 15:23:39 +0700

    change

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation,  } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useRef, useLayoutEffect } from "react";
 
 import Header from "@/components/layout/Header";
@@ -13,19 +13,19 @@ export default function Layout() {
 	}, [location.pathname]);
 
 	return (
-		<div className="main-layout flex flex-col items-center w-full h-full bg-dark-100">
+		<div className="main-layout flex flex-col items-center w-full h-dvh bg-dark-100">
 			<header className="flex justify-center w-full h-max bg-dark-100">
-				<Header/>
+				<Header />
 			</header>
-			<div 
-				ref={bodyContainerRef} 
+			<div
+				ref={bodyContainerRef}
 				className="relative body-container no-scrollbar flex justify-content grow w-full overflow-auto"
 			>
-				<Outlet context={bodyContainerRef}/>
+				<Outlet context={bodyContainerRef} />
 			</div>
 			<footer className="flex justify-center w-full h-max bg-dark-300">
-				<Footer/>
+				<Footer />
 			</footer>
-		</div>	
-	)
+		</div>
+	);
 }
