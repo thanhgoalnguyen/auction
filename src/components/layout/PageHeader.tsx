0d00c58702d7bd0cdb92855621e9e96d25ab2b03
@nguyenngocthanh137 <nguyenngocthanh137@gmail.com>
2026-01-09 02:39:52 +0700

    build

--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -3,43 +3,47 @@ import { Link } from "react-router-dom";
 import { ROUTE_PATH } from "@/data/demo";
 
 type PageHeaderProps = {
-	isTopPage?: boolean
-	isAuth?: boolean
-	title?: string
-}
+	isTopPage?: boolean;
+	isAuth?: boolean;
+	title?: string;
+};
 
-export default function PageHeader({ isTopPage = false, isAuth = false, title = "" }: PageHeaderProps) {
+export default function PageHeader({
+	isTopPage = false,
+	isAuth = false,
+	title = "",
+}: PageHeaderProps) {
 	const handleRenderContent = () => {
 		if (isTopPage) {
 			if (isAuth) {
 				return <p>Welcome Back</p>;
 			} else {
-				return <>
-					<Link 
-						to={ROUTE_PATH?.USER_REGISTRATION} 
-						className="text-inherit no-underline"
-					>
-						新規登録
-					</Link>
-					<Link 
-						to={ROUTE_PATH?.LOGIN} 
-						className="text-inherit no-underline"
-					>
-						ログイン
-					</Link>
-				</>;
+				return (
+					<>
+						<Link
+							to={ROUTE_PATH?.USER_REGISTRATION}
+							className="text-inherit no-underline"
+						>
+							新規登録
+						</Link>
+						<Link to={ROUTE_PATH?.LOGIN} className="text-inherit no-underline">
+							ログイン
+						</Link>
+					</>
+				);
 			}
 		} else {
 			return <p>{title}</p>;
 		}
-	}
+	};
 
 	return (
 		(isTopPage || title) && (
-			<div className={`page-header flex justify-center gap-[22px] w-full pt-[11px] pb-[15px] text-[11px] leading-[13px] text-white bg-dark-300 ${isTopPage && "sticky top-0 z-[2]"}`}>
+			<div
+				className={`page-header flex justify-center gap-[22px] w-full pt-[11px] pb-[15px] text-[11px] leading-[13px] text-white bg-dark-300 ${isTopPage && !isAuth && "sticky top-0 z-[2]"}`}
+			>
 				{handleRenderContent()}
 			</div>
 		)
-	)
+	);
 }
-  
